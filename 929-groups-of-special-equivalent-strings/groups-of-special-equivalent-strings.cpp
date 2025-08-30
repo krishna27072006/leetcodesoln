524eeae049294bcf8aab448d805b130368967c39
Krishna Mundada <krishnamundada2006@gmail.com>
2025-08-30 12:14:25 +0530

    Time: 2 ms (69.65%) | Memory: 12.2 MB (36.74%) - LeetSync

---
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& words) {
+        set<pair<string,string>> s;
+        for(int i=0;i<words.size();++i){
+            string e="";
+            string o="";
+            for(int j=0;j<words[i].size();++j){
+                if(j%2==0){
+                    e+=words[i][j];
+                }else{
+                    o+=words[i][j];
+                }
+            }
+            sort(e.begin(),e.end());
+            sort(o.begin(),o.end());
+            s.insert({e,o});
+        }
+        return s.size();
+        
+    }
+};
