a9f87782ffc39c0ad147c883c20cc71e82d4fbd3
Krishna Mundada <krishnamundada2006@gmail.com>
2025-08-31 16:51:40 +0530

    Time: 31 ms (67.62%) | Memory: 14.5 MB (26.43%) - LeetSync

---
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkIfCanBreak(string s1, string s2) {
+        bool ans1=true;
+        bool ans2=true;
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        for(int i=0;i<s1.size();++i){
+            if((int)s1[i]<(int)s2[i]){
+                ans1=false;
+                break;
+            }
+        }
+        for(int i=0;i<s1.size();++i){
+            if((int)s2[i]<(int)s1[i]){
+                ans2=false;
+                break;
+            }
+        }
+        return (ans1 || ans2);
+    }
+};
