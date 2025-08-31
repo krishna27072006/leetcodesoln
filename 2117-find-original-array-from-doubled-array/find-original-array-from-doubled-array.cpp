2aa5a4d91ed01756e6b18cd6ba1a5cbb64101250
Krishna Mundada <krishnamundada2006@gmail.com>
2025-08-31 18:40:05 +0530

    Time: 145 ms (55.12%) | Memory: 145.7 MB (48.24%) - LeetSync

---
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& c) {
+        if(c.size()%2!=0) return {};
+        unordered_map<int,int> m;
+        for(int i=0;i<c.size();++i){
+            m[c[i]]+=1;
+        }
+        sort(c.begin(),c.end());
+        vector<int> v;
+        for(auto t :c){
+            if(m[t]==0) continue;
+            if(m[t*2]==0) return {};
+            v.push_back(t);
+            m[t]--;
+            m[t*2]--;
+        }
+        return  v;
+    
+        
+    }
+};
