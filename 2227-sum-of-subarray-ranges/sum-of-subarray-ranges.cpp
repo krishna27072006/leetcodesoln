6ddf16e0e8a5398b2edec02a244c3bba24ac0c21
Krishna Mundada <krishnamundada2006@gmail.com>
2026-05-13 01:04:15 +0530

    Time: 6 ms (46.40%) | Memory: 15.8 MB (40.90%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -9,7 +9,6 @@ public:
         stack<int> ng;
         stack<int> pg;
 
-        // NEXT GREATER
         for(int i=n-1;i>=0;--i){
             while(!ng.empty() && nums[ng.top()]<=nums[i]){
                 ng.pop();
@@ -24,7 +23,6 @@ public:
             ng.push(i);
         }
 
-        // PREVIOUS GREATER
         for(int i=0;i<n;++i){
             while(!pg.empty() && nums[pg.top()]<nums[i]){
                 pg.pop();
@@ -41,7 +39,6 @@ public:
 
         long long ans=0;
 
-        // SUM OF MAXIMUMS
         for(int i=0;i<n;i++){
             long long left=i-pge[i];
             long long right=nge[i]-i;
@@ -55,7 +52,6 @@ public:
         stack<int> ns;
         stack<int> p;
 
-        // NEXT SMALLER
         for(int i=n-1;i>=0;--i){
             while(!ns.empty() && nums[ns.top()]>=nums[i]){
                 ns.pop();
@@ -70,7 +66,6 @@ public:
             ns.push(i);
         }
 
-        // PREVIOUS SMALLER
         for(int i=0;i<n;++i){
             while(!p.empty() && nums[p.top()]>nums[i]){
                 p.pop();
@@ -87,7 +82,6 @@ public:
 
         long long ans1=0;
 
-        // SUM OF MINIMUMS
         for(int i=0;i<n;i++){
             long long left=i-pse[i];
             long long right=nse[i]-i;
