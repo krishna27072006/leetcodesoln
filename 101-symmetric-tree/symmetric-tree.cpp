c830a1ba24770846e58530b863e9f4759f69d6c6
Krishna Mundada <krishnamundada2006@gmail.com>
2026-06-06 17:55:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (25.70%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,36 +13,21 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         queue<TreeNode*> q;
-        vector<vector<int>> v;
-        q.push(root);
+        q.push(root->left);
+        q.push(root->right);
         while(!q.empty()){
-            vector<int> p;
-            int n=q.size();
-            for(int i=0;i<n;++i){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node == nullptr){
-                    p.push_back(INT_MIN);
-                    continue;
-                }
-                p.push_back(node->val);
-                q.push(node->left);
-                q.push(node->right);
-            }
-            v.push_back(p);
+            TreeNode* a=q.front();
+            q.pop();
+            TreeNode* b=q.front();
+            q.pop();
+            if(!a && !b) continue;
+            if(!a || !b) return false;
+            if(a->val != b->val) return false;
+            q.push(a->left);
+            q.push(b->right);
+            q.push(a->right);
+            q.push(b->left);
         }
-        for(int i=1;i<v.size();++i){
-            if(v[i].size()%2!=0){
-                return false;
-            }else{
-                int n=v[i].size();
-                for(int j=0;j<(n/2);++j){
-                    if(v[i][j]!=v[i][n-1-j]){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;  
+        return true;
     }
 };
