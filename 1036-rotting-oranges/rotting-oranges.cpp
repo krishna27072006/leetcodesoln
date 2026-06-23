500aa22d3f0c006b39abf3615fd542e500dfd2f6
Krishna Mundada <krishnamundada2006@gmail.com>
2026-06-23 15:26:18 +0530

    Time: 1 ms (37.83%) | Memory: 16.7 MB (86.04%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& g) {
+
+        int n = g.size();
+        int m = g[0].size();
+
+        int fresh = 0;
+        queue<pair<int,int>> q;
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+
+                if(g[i][j] == 2){
+                    q.push({i,j});
+                }
+                else if(g[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        while(!q.empty() && fresh > 0){
+
+            ans++;
+
+            vector<pair<int,int>> v;
+
+            while(!q.empty()){
+
+                auto t = q.front();
+                q.pop();
+
+                int i = t.first;
+                int j = t.second;
+
+                if(i + 1 < n && g[i+1][j] == 1){
+                    v.push_back({i+1, j});
+                    g[i+1][j] = 2;
+                    fresh--;
+                }
+
+                if(i - 1 >= 0 && g[i-1][j] == 1){
+                    v.push_back({i-1, j});
+                    g[i-1][j] = 2;
+                    fresh--;
+                }
+
+                if(j + 1 < m && g[i][j+1] == 1){
+                    v.push_back({i, j+1});
+                    g[i][j+1] = 2;
+                    fresh--;
+                }
+
+                if(j - 1 >= 0 && g[i][j-1] == 1){
+                    v.push_back({i, j-1});
+                    g[i][j-1] = 2;
+                    fresh--;
+                }
+            }
+
+            for(auto t : v){
+                q.push(t);
+            }
+        }
+
+        return (fresh == 0) ? ans : -1;
+    }
+};
