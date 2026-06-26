f22696ba5ea711685904cf0ad40c6b6f17894d8a
Krishna Mundada <krishnamundada2006@gmail.com>
2026-06-26 14:57:21 +0530

    Time: 33 ms (21.10%) | Memory: 23.1 MB (32.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,46 +3,35 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<pair<int,int>> v;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(grid[i][j]=='1'){
-                    v.push_back({i,j});
-                }
-            }
-        }
         int delrow[4] = {-1,0,1,0};
         int delcol[4] = {0,1,0,-1};
         int ans=0;
-        vector<vector<char>> vis(n,vector<char>(m,'0'));
-        for(int i=0;i<v.size();++i){
-            int l=v[i].first;
-            int k=v[i].second;
-            queue<pair<int,int>> q;
-            if(vis[l][k]=='0'){
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]=='1'){
+                    grid[i][j]='0';
+                    queue<pair<int,int>> q;
+            
                 ans++;
-                vis[l][k]='1';
-                q.push({l,k});
+                q.push({i,j});
                 while(!q.empty()){
                     int r=q.front().first;
                     int c=q.front().second;
                     q.pop();
-                    for(int i=0;i<4;++i){
-                        int rr=r+delrow[i];
-                        int cc=c+delcol[i];
-                        if(rr>=0 && rr<n && cc>=0 && cc<m && vis[rr][cc]=='0' && grid[rr][cc]=='1'){
+                    for(int k=0;k<4;++k){
+                        int rr=r+delrow[k];
+                        int cc=c+delcol[k];
+                        if(rr>=0 && rr<n && cc>=0 && cc<m && grid[rr][cc]=='1'){
                             q.push({rr,cc});
-                            vis[rr][cc]='1';
+                            grid[rr][cc]='0';
                         }
                     }
 
                 }
-
-
             }
         }
+        }
         return ans;
-        
         
     }
 };
