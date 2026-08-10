b6e0b260a2f603f0a67082aec4296635d49656fd
Krishna Mundada <krishnamundada2006@gmail.com>
2026-08-11 00:47:12 +0530

    Time: 2 ms (36.03%) | Memory: 16.3 MB (62.63%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -22,7 +22,27 @@ public:
         int n=grid[0].size();
         int mini=1e5;
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,grid,dp);
+        // return f(m-1,n-1,grid,dp);
+         
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                    continue;
+                }
+                int right=1e9;
+                int down=1e9;
+                if(j>0){
+                    right=dp[i][j-1];
+                }
+                if(i>0){
+                    down=dp[i-1][j];
+                }
+                dp[i][j]=grid[i][j]+min(right,down);
+
+            }
+        }
+        return dp[m-1][n-1];
 
         
     }
