6c433812ad2fad88d928c7d5cb380450d52e72a2
Krishna Mundada <krishnamundada2006@gmail.com>
2026-08-12 16:05:49 +0530

    Time: 47 ms (35.23%) | Memory: 49.5 MB (26.81%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(int i ,int amount,vector<int> & coin,vector<vector<int>> &dp ){
+        if(amount==0){
+            return 0;
+        }
+        if(i==0){
+            if(amount%coin[0]==0){
+                return amount/coin[0];
+            }else{
+                return 1e9;
+            }
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+
+        int not_take=f(i-1,amount,coin,dp);
+        int same_take=1e9;
+        if(coin[i]<=amount){
+            same_take=1+f(i,amount-coin[i],coin,dp);
+        }
+        return dp[i][amount]=min(not_take,same_take);
+        
+
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans = f(n-1,amount,coins,dp);
+
+        if (ans >= 1e9)
+            return -1;
+
+        return ans;
+    }
+};
